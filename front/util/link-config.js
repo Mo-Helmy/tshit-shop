a2fb8501da970d38523e3efa5fc489c803229e65
Mo Helmy <helmysmile3@gmail.com>
2023-02-12 18:08:29 +0200

    update api link

--- front/util/link-config.js
+++ front/util/link-config.js
@@ -11,7 +11,7 @@ if (environment === 'development') {
   apiUrl = 'http://localhost:4000';
   clientUrl = 'http://localhost:3000';
 } else {
-  apiUrl = 'http://localhost:4000';
+  apiUrl = 'http://godzilla.us-east-1.elasticbeanstalk.com/';
   clientUrl = 'http://localhost:3000';
 }
 
